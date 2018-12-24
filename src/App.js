606d766de7f0ed8208013595089fb876d3467dbc
Bianca Oparaji <chinwebiancaoparaji@biancas-air.attlocal.net>
2018-12-23 21:31:37 -0600

    Tha First Stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BHacks from './components/BHacksPart/BHacks';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BHacks />
+      </div>
+    );
+  }
+}
+
+export default App;
